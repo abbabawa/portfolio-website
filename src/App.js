9e476d771c12e97ff014c35995a0d3ba105ed591
Bawa Abba <abbabawa6@gmail.com>
2021-09-21 10:57:21 +0100

    Navbar and Sidebar created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import Navigation from './components/Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SideBar from './components/SideBar';
+import { Route, Switch } from 'react-router';
 
 function App() {
+
+	let menuItems = [
+		{
+			id: 1,
+			title: 'Home',
+			icon: 'Home'
+		},
+		{
+			id: 2,
+			title: 'About',
+			icon: 'Home'
+		},
+		{
+			id: 3,
+			title: 'Portfolio',
+			icon: 'Home'
+		},
+		{
+			id: 4,
+			title: 'Projects',
+			icon: 'Home'
+		}
+	]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="mx-0 px-0">
+		<Navigation menuItems={menuItems} />
+		<Row>
+			<Col xs="1" className="border text-center d-none">
+				<SideBar menuItems={menuItems} />
+			</Col>
+			
+		</Row>
+    </Container>
   );
 }
 
